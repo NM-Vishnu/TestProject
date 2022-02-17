0826846e715dd6143522aeed0cbfbc0b3034ff05
Vishnu <vishnuram.nagarajan@gmail.com>
2022-02-17 00:15:44 +0000

    Constructor Parm

--- Test Project/src/com/javalearning/ConstructorChaining.java
+++ Test Project/src/com/javalearning/ConstructorChaining.java
@@ -4,10 +4,13 @@ public class ConstructorChaining {
 	public String sName;
 	public int sMarks;
 	
+	//default constructor
 	ConstructorChaining(){
+		//this will call the constructor with string parm
 		this("Vishnu");
 	}
 	ConstructorChaining(String name){
+		//this will call the constructor with string and int parm
 		this(name,70);
 	}
 	ConstructorChaining(String name, int mark){
